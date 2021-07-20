fd2f8d9e5937ba7b3f5accc0d709c511afa3dca5
BahadirIren <bahadiriren@gmail.com>
2021-07-20 07:58:30 +0300

    Initial commit

---
+++ UdemyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string InnerBarcode { get; set; }
+
+        // Category sinifina referans veriyoruz
+        // Entity Framework bu Category uzerinden inherit kullanarak tracking yapacak,
+        // degisiklikleri izleyecek bu yuzden virtual kullandik
+        public virtual Category Category { get; set; } 
+
+    }
+}
